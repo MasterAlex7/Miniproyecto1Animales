7c77e89f3c719264bd78c6eb43d46588eee35dbb
Quinnpapasote89 <admaor2000@gmail.com>
2022-03-08 20:28:31 -0600

    Juego Antes de Terminar Estilo

--- js/funciones.js
+++ js/funciones.js
@@ -1,21 +1,28 @@
-function iniciar(){
+function iniciar() {
     window.location.href = "login.html"
 }
 
-function jugar(){
-    localStorage.nombre = document.getElementById("nombre").value;
-    window.location.href = "juego.html"
+function jugar() {
+    var nombre = document.getElementById("nombre").value;
+    if (nombre.trim() == "") {
+        alert("Ups, debes ingresar un nombre para jugar");
+        setTimeout(() => { window.location.href = "login.html"(); }, 3000);
+    } else {
+        localStorage.nombre = nombre;
+        window.location.href = "juego.html"
+    }
+
 }
 
 
 
-window.onload=function(){
+window.onload = function () {
     var canvas = document.getElementById('lienzo');
     if (canvas && canvas.getContext) {
         var ctx = canvas.getContext('2d');
         if (ctx) {
             ctx.beginPath();
-    
+
             ctx.font = "35pt Comic Sans MS, Comic Sans, cursive";
             ctx.fillStyle = "yellow";
             ctx.fillText("Bienvenido a Animal Choose", 100, 120);
@@ -28,10 +35,10 @@ window.onload=function(){
             ctx.fillStyle = "pink";
             ctx.fillText("en su Habitat para ganar", 220, 430);
 
-            var imag=new Image();
-            imag.src="media/logo.png";
-            imag.onload=function(){
-                ctx.drawImage(imag,190,190,328,125);
+            var imag = new Image();
+            imag.src = "media/logo.png";
+            imag.onload = function () {
+                ctx.drawImage(imag, 190, 190, 328, 125);
             }
 
         }
